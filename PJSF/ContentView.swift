1c510079d2d5749c1b224ca52b16870f72a00e9a
Lim Jun Rui <lim_jun_rui@s2021.ssts.edu.sg>
2023-05-30 17:13:53 +0800

    Initial Commit

---
+++ PJSF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PJSF
+//
+//  Created by Lim Jun Rui on 30/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PJSF/PJSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PJSFApp.swift
+//  PJSF
+//
+//  Created by Lim Jun Rui on 30/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PJSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
